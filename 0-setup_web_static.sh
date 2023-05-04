bd95f3ac2e85df0b54a29a55afd7d18ac89a3209
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-05-03 22:48:44 -0700

    deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 # Script will set up web servers
 
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Testing Ngnix Configuration</title>
-  </head>
-  <body>
-     School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
